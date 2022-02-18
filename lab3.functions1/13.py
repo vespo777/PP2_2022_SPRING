0e0063c06112ad3a7292ad966abd1b0a0cf8ea63
vespo777 <87870672+vespo777@users.noreply.github.com>
2022-02-18 21:47:59 +0600

    Update 13.py

--- lab3.functions1/13.py
+++ lab3.functions1/13.py
@@ -1,3 +1,27 @@
-def guess_the_num()
-    print("Hello! What is your name?")
-    name = input()
+import random
+def game(r, cnt, name):
+    print("Take a guess.")
+    b = int(input())
+    if b > r:
+        print("Your guess is too high.")
+        cnt+=1
+        return game(r, cnt, name)
+    elif b < r:
+        print("Your guess is too low.")
+        cnt+=1
+        return game(r, cnt, name)
+    elif b == r:
+        print("Good job, " + name +"! You guessed my number in " + str(cnt)+" guesses!")
+
+
+
+
+
+print("Hello! What is your name?")
+a=input()
+
+
+print("Well, " + str(a) + ", I am thinking of a number between 1 and 20.")
+
+c = random.randrange(1,20)
+game(c, 0, a)
