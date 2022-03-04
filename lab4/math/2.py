8c925f4b1352f8c3974cee1af7567a101e92514f
vespo777 <87870672+vespo777@users.noreply.github.com>
2022-03-04 15:50:04 +0600

    Add files via upload

---
+++ lab4/math/2.py
@@ -0,0 +1,5 @@
+import math
+h = int(input("Height: "))
+a = int(input("Base, first value: "))
+b = int(input("Base, second value: "))
+print("Expected Output: ", (h * (a + b)) / 2)
---
+++ lab4/math/4.py
@@ -0,0 +1,5 @@
+import math
+
+a = int(input("Length of base: "))
+b = int(input("Height of parallelogram: "))
+print("Expected Output:", a*b)
