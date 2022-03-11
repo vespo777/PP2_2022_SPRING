852db0e5e56835acf09a01d82c9eab42399b729e
vespo777 <87870672+vespo777@users.noreply.github.com>
2022-03-11 21:57:48 +0600

    Delete 5.py

--- 5.py
+++
@@ -1,9 +0,0 @@
-import re
-def text_match(text):
-        patterns = 'a.*?b$'
-        if re.search(patterns,  text):
-                return 'matching'
-        else:
-                return('not matching')
-
-                
