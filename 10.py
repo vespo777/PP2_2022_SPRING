0b49c0b316ba0d1ce96d9b447f9406fc660b8ea3
vespo777 <87870672+vespo777@users.noreply.github.com>
2022-03-11 21:57:37 +0600

    Delete 10.py

--- 10.py
+++
@@ -1,8 +0,0 @@
-import re
-def snake_case(s):
-  return '_'.join(
-    re.sub('([A-Z][a-z]+)', r' \1',
-    re.sub('([A-Z]+)', r' \1',
-    s.replace('-', ' '))).split()).lower()
-
-print(snake_case('AlmatyCity'))
