e25c8f28d3f2a09b20dd46c7aac5772b5ac57157
vespo777 <87870672+vespo777@users.noreply.github.com>
2022-03-18 21:47:56 +0600

    Add files via upload

---
+++ lab6/file handling/4.py
@@ -0,0 +1,8 @@
+file = open(r"C:\Users\tileu\Desktop\istok\lab6\text.txt")
+
+cnt = 0
+
+for i in file:
+        pass
+        cnt += 1
+print(cnt)
---
+++ lab6/file handling/5.py
@@ -0,0 +1,4 @@
+file = open('text.txt', 'w')
+
+
+file.write('[coca cola, pepsi]')
